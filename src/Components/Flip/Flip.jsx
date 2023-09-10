1c1b72e7c7c0a2eea5379bfd5e2f52b27905a886
juanigarciadev <juanigarciadev@gmail.com>
2023-09-10 16:50:32 -0300

    added flip

--- src/Components/Flip/Flip.jsx
+++ src/Components/Flip/Flip.jsx
@@ -6,43 +6,54 @@ const Flip = ({
   editedRef,
 }) => {
   return (
-    <section className="grid grid-cols-2 h-screen gap-4 bg-black text-white p-8">
-      <div className="flex flex-col gap-8 justify-center items-center relative bg-neutral-800 rounded-xl">
-        <h3 className="absolute top-4 font-bold">Input</h3>
-        {selectedImage && (
-          <img
-            className="max-h-[40%]"
-            src={URL.createObjectURL(selectedImage)}
-            alt=""
-          />
-        )}
-        {selectedImage === null ? (
-          <input
-            type="file"
-            name="myImage"
-            onChange={(e) => setSelectedImage(e.target.files[0])}
-          />
-        ) : (
-          <button
-            className="btn cursor-pointer absolute bottom-4"
-            onClick={() => setSelectedImage(null)}
-          >
-            Remove image
-          </button>
-        )}
-      </div>
-      <div className="flex justify-center items-center relative bg-neutral-800 rounded-xl">
-        <h3 className="absolute top-4 font-bold">Output</h3>
+    <section className="min-h-screen bg-black text-white">
+      <section className="grid grid-cols-2 grid-rows-1 gap-4 h-[50vh] p-8 lg:flex lg:flex-col lg:h-[80vh]">
         {selectedImage !== null ? (
-          <img
-            className="max-h-[40%] contrast-200"
-            ref={editedRef}
-            src={URL.createObjectURL(selectedImage)}
-          />
+          <div className="flex flex-col p-4 gap-4 h-full bg-neutral-800 rounded-xl">
+            <h3 className="font-bold w-32 bg-neutral-900 p-2">
+              Original image
+            </h3>
+            <div className="flex justify-center w-full bg-black h-[70%]">
+              <img
+                className="h-full"
+                src={URL.createObjectURL(selectedImage)}
+                alt={selectedImage}
+              />
+            </div>
+            <button className="btn" onClick={() => setSelectedImage(null)}>
+              Remove image
+            </button>
+          </div>
         ) : (
-          <h3>No image selected</h3>
+          <div className="h-32 bg-neutral-800 rounded-xl flex flex-col p-2 gap-2">
+            <h3 className="font-bold w-32 bg-neutral-900 p-2">
+              Original image
+            </h3>
+            <input
+              type="file"
+              name="myImage"
+              onChange={(e) => setSelectedImage(e.target.files[0])}
+            />
+          </div>
         )}
-      </div>
+        {selectedImage !== null ? (
+          <div className="flex flex-col gap-4 h-full p-4 bg-neutral-800 rounded-xl">
+            <h3 className="font-bold w-32 bg-neutral-900 p-2">Flipped image</h3>
+            <div className="flex justify-center w-full bg-black h-[70%]">
+              <img
+                className="h-full horizontal-flip"
+                src={URL.createObjectURL(selectedImage)}
+                alt={selectedImage}
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2 p-2 h-32 bg-neutral-800 rounded-xl">
+            <h3 className="font-bold w-32 bg-neutral-900 p-2">Flipped image</h3>
+            <h3>No image selected</h3>
+          </div>
+        )}
+      </section>
     </section>
   );
 };
